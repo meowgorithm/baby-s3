2ba8d6a4ee4529a15b3cc69529892f5996ffb2a1
Christian Rocha <christian@rocha.is>
2018-05-14 22:13:25 -0400

    Support for object and bucket existance + fetching object metadata

--- babys3.go
+++ babys3.go
@@ -13,19 +13,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-// ObjectInfo contains information about an object. It's a simplified version
-// of `aws/aws-sdk-go/service/s3/HeadObjectOutput`.
-//
-// For info on s3.HeadObjectOutput see:
-// https://docs.aws.amazon.com/sdk-for-go/api/service/s3/#HeadObjectOutput
-type ObjectInfo struct {
-	ContentDisposition string
-	Encoding           string
-	Language           string
-	Length             string
-	MimeType           string
-}
-
 // A template for a generally publicly open bucket policy. Note the `%s` which
 // should be replaced by the bucket name.
 const publicReadBucketPolicy = `{
@@ -42,54 +29,73 @@ const publicReadBucketPolicy = `{
 }`
 
 // AWS makes us do this before we do anything else
-func newSession() (svc *session.Session, err error) {
+func newSession() (svc *s3.S3, err error) {
 	region := os.Getenv("AWS_REGION")
 	if region == "" {
 		return nil, errors.New("environment variable `AWS_REGION` not set")
 	}
 
-	return session.NewSession(&aws.Config{
+	s, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
 	})
+	if err != nil {
+		return nil, err
+	}
+
+	return s3.New(s), nil
 }
 
 // BucketExists determines if a given bucket exists
-func BucketExists(name string) (bool, error) {
-	s, err := newSession()
+func BucketExists(name string) (exists bool, err error) {
+	svc, err := newSession()
 	if err != nil {
 		return false, err
 	}
-
-	svc := s3.New(s)
 
 	_, err = svc.HeadBucket(&s3.HeadBucketInput{
 		Bucket: aws.String(name),
 	})
 
 	if err != nil {
-		if aErr, ok := err.(awserr.Error); ok {
-			switch aErr.Code() {
+		if awsErr, ok := err.(awserr.Error); ok {
+			switch awsErr.Code() {
 			case s3.ErrCodeNoSuchBucket:
+				// Bucket doesn't exist: no error.
+				//
+				// AWS tells us this is the error to expect, though we
+				// personally didn't encounter it in our testing.
+				return false, nil
+			case "NotFound":
+				// Bucket doesn't exist: no error.
+				//
+				// AWS seems to prefer this error over the former one, which is
+				// contratictory to their docs as far as we can tell.
 				return false, nil
 			default:
-				return false, aErr
+				// Error.
+				//
+				// No way to tell if the bucket doesn't exist because we hit
+				// some other error.
+				return false, awsErr
 			}
-		} else {
-			return false, err
 		}
+
+		// This should probably never happen, but we should keep an eye out for
+		// it anyway.
 		return false, err
 	}
+
+	// Bucket exists
 	return true, nil
 }
 
-// CreateBucket creates a new S3 bucket.
+// CreateBucket creates a new S3 bucket. At the time of writing (2018/05/14)
+// creating a bucket that exists and is owned by you will succeed silently.
 func CreateBucket(name string) error {
-	s, err := newSession()
+	svc, err := newSession()
 	if err != nil {
 		return err
 	}
-
-	svc := s3.New(s)
 
 	_, err = svc.CreateBucket(&s3.CreateBucketInput{
 		Bucket: aws.String(name),
@@ -101,18 +107,14 @@ func CreateBucket(name string) error {
 	return svc.WaitUntilBucketExists(&s3.HeadBucketInput{
 		Bucket: aws.String(name),
 	})
-
-	return err
 }
 
 // MakeBucketPublic makes a bucket publicly readable over HTTP/HTTPS
 func MakeBucketPublic(name string) error {
-	s, err := newSession()
+	svc, err := newSession()
 	if err != nil {
 		return err
 	}
-
-	svc := s3.New(s)
 
 	_, err = svc.PutBucketPolicy(&s3.PutBucketPolicyInput{
 		Bucket: aws.String(name),
@@ -124,12 +126,10 @@ func MakeBucketPublic(name string) error {
 
 // DeleteBucket does exactly that
 func DeleteBucket(name string) error {
-	s, err := newSession()
+	svc, err := newSession()
 	if err != nil {
 		return err
 	}
-
-	svc := s3.New(s)
 
 	_, err = svc.DeleteBucket(&s3.DeleteBucketInput{
 		Bucket: aws.String(name),
@@ -144,37 +144,43 @@ func DeleteBucket(name string) error {
 }
 
 // HeadObject fetches simple information about ab object without actually
-// fetching the object
+// fetching the object.
+//
+// For information on what exactly is returned see:
+// https://docs.aws.amazon.com/sdk-for-go/api/service/s3/#HeadObjectOutput
 func HeadObject(bucket, filename string) (*s3.HeadObjectOutput, error) {
-	s, err := newSession()
+	svc, err := newSession()
 	if err != nil {
 		return nil, err
 	}
 
-	svc := s3.New(s)
-
-	return svc.HeadObject(&s3.HeadObjectInput{
+	h, err := svc.HeadObject(&s3.HeadObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(filename),
 	})
+	if err != nil {
+		return nil, err
+	} else if h == nil {
+		return nil, fmt.Errorf("received no error, but also recieved no information about the object '%s' in bucket '%s'",
+			filename, bucket)
+	}
+	return h, err
 }
 
 // ObjectExists returns true if the object exists on S3, and false if not
-func ObjectExists(bucket, filename string) bool {
+func ObjectExists(bucket, filename string) (exists bool, err error) {
 	if _, err := HeadObject(bucket, filename); err != nil {
-		return false
+		return false, err
 	}
-	return true
+	return true, nil
 }
 
 // UploadObject uploads a given file to an S3 bucket
 func UploadObject(bucket, filename string, data []byte) (err error) {
-	s, err := newSession()
+	svc, err := newSession()
 	if err != nil {
 		return err
 	}
-
-	svc := s3.New(s)
 
 	_, err = svc.PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(bucket),
@@ -197,12 +203,10 @@ func UploadObjectAsBase64(bucket, filename, base64Data string) (err error) {
 
 // DeleteObject deletes an object from an S3 bucket
 func DeleteObject(bucket, filename string) error {
-	s, err := newSession()
+	svc, err := newSession()
 	if err != nil {
 		return err
 	}
-
-	svc := s3.New(s)
 
 	deleteInput := s3.DeleteObjectInput{
 		Bucket: aws.String(bucket),
--- babys3_test.go
+++ babys3_test.go
@@ -2,6 +2,8 @@ package babys3
 
 import (
 	"testing"
+
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 const (
@@ -16,60 +18,105 @@ func TestNewSession(t *testing.T) {
 	}
 }
 
-func TestAllTheS3Stuff(t *testing.T) {
-
-	bucketName := RandomBucketName()
-
-	t.Run("eh", func(t *testing.T) {
-		if _, err := HeadObject(bucketName, "eh"); err != nil {
-			t.Errorf("wut %s\n", err)
+func TestBucketOperations(t *testing.T) {
+	n := "baby-s3-test-" + RandomBucketName()
+	t.Run("create bucket", testCreateBucket(n))
+	t.Run("bucket exists", testBucketExists(n))
+	t.Run("make bucket public", func(t *testing.T) {
+		if err := MakeBucketPublic(n); err != nil {
+			t.Errorf("tried to make bucket '%s' public, but could not:", err)
 		}
 	})
+	t.Run("delete bucket", testDeleteBucket(n))
+}
 
-	t.Run("test create bucket", func(t *testing.T) {
-		if err := CreateBucket(bucketName); err != nil {
-			t.Errorf("error creating bucket named %s: %s\n", bucketName, err)
-		}
-	})
-
-	t.Run("test make bucket public", func(t *testing.T) {
-		if err := MakeBucketPublic(bucketName); err != nil {
-			t.Errorf("error creating bucket named %s: %s\n", bucketName, err)
-		}
-	})
-
+func TestObjectOperations(t *testing.T) {
+	bucketName := "baby-s3-test-" + RandomBucketName()
 	textFilename := "meow.txt"
+	textFileBytes := []byte("here, kitty kitty")
+
+	t.Run("create bucket", testCreateBucket(bucketName))
 
 	t.Run("test upload file as bytes", func(t *testing.T) {
-		err := UploadObject(bucketName, textFilename, []byte("here, kitty kitty"))
+		err := UploadObject(bucketName, textFilename, textFileBytes)
 		if err != nil {
-			t.Error("couldn't upload bytes", err)
+			t.Error("couldn't upload object:", err)
 		}
 	})
 
 	// Also testing that we can put stuff in a "directory"
-	imageFilename := "pixels/pixel.png"
+	imageFilename := "pixel/pixel.png"
 
 	t.Run("test upload file as base 64", func(t *testing.T) {
 		if err := UploadObjectAsBase64(bucketName, imageFilename, pixelBase64Data); err != nil {
-			t.Error("error uploading to S3", err)
+			t.Error("couldn't upload object (from base64)", err)
+		}
+	})
+
+	t.Run("test object exists", func(t *testing.T) {
+		if exists, err := ObjectExists(bucketName, textFilename); err != nil {
+			t.Error("could not check if object exists", err)
+		} else if !exists {
+			t.Errorf("object '%s' doesn't exist in bucket '%s', but it was supposed to be there: %s\n",
+				textFilename, bucketName, err)
+		}
+	})
+
+	t.Run("test head object", func(t *testing.T) {
+		var (
+			info *s3.HeadObjectOutput
+			err  error
+		)
+		if info, err = HeadObject(bucketName, textFilename); err != nil {
+			t.Error("tried to get information about object but we could not:", err)
+		}
+
+		bytesReturned := *info.ContentLength
+		actualBytes := int64(len(textFileBytes))
+
+		if actualBytes != bytesReturned {
+			t.Errorf("curious. S3 says the file is %d bytes, when in fact it's %d. That's an error.", bytesReturned, actualBytes)
 		}
 	})
 
 	t.Run("test delete files", func(t *testing.T) {
 		if err := DeleteObject(bucketName, textFilename); err != nil {
-			t.Error("error deleting text file from S3", err)
+			t.Errorf("error deleting text file '%s' from bucket '%s:' %s\n", textFilename, bucketName, err)
 			return
 		}
 		if err := DeleteObject(bucketName, imageFilename); err != nil {
-			t.Error("error deleting image from S3", err)
+			t.Errorf("error deleting image '%s' from bucket '%s': %s\n", imageFilename, bucketName, err)
 			return
 		}
 	})
 
-	t.Run("test delete bucket", func(t *testing.T) {
-		if err := DeleteBucket(bucketName); err != nil {
-			t.Errorf("error deleting bucket named %s: %s\n", bucketName, err)
+	t.Run("test delete bucket", testDeleteBucket(bucketName))
+}
+
+// Test helpers
+
+func testCreateBucket(name string) func(*testing.T) {
+	return func(t *testing.T) {
+		if err := CreateBucket(name); err != nil {
+			t.Errorf("tried to create bucket '%s' but could not: %s", name, err)
 		}
-	})
+	}
+}
+
+func testBucketExists(name string) func(*testing.T) {
+	return func(t *testing.T) {
+		if exists, err := BucketExists(name); err != nil {
+			t.Errorf("tried to see if bucket '%s' exists but could not: %s\n", name, err)
+		} else if !exists {
+			t.Errorf("bucket '%s' does not exist: %s\n", name, err)
+		}
+	}
+}
+
+func testDeleteBucket(name string) func(*testing.T) {
+	return func(t *testing.T) {
+		if err := DeleteBucket(name); err != nil {
+			t.Errorf("tried to delete bucket '%s' but could not: %s", name, err)
+		}
+	}
 }
